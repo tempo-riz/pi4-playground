65a9bccd031bba8ae8a2a99ff2675afa4f176d63
Thibault <thibaultchatillon@gmail.com>
2023-11-10 12:41:00 +0100

    test websocket

--- server-pi/server.py
+++ server-pi/server.py
@@ -1,13 +1,59 @@
 import asyncio
 import websockets
+import time
+from gpiozero import CamJamKitRobot, DistanceSensor
+
+# Define GPIO pins to use for the distance sensor
+pin_trigger = 17
+pin_echo = 18
+
+robot = CamJamKitRobot()
+sensor = DistanceSensor(echo=pin_echo, trigger=pin_trigger)
+
+
+# Variables
+min_distance = 30.0
+left_speed = 0.5
+right_speed = 0.5
+
+forward = (left_speed, right_speed)
+backward = (-left_speed, -right_speed)
+left_turn = (left_speed, -right_speed)
+right_turn = (-left_speed, right_speed)
+
+def isInRange():
+    dist = sensor.distance * 100
+    print("distance: " + str(dist))
+    return dist < min_distance
+
+
 
 async def handle_connection(websocket, path):
     try:
         async for message in websocket:
-            # Here, you can handle the received message (direction)
-            print(f"Received direction: {message}")
+            print(f"Received: {message}")
+            # forward, backward, left, right, stop 
+            match message:
+                case "forward":
+                    if not isInRange():
+                        robot.value = forward
+                    else:
+                        robot.stop()
+                case "backward":
+                    robot.value = backward
+                case "right":
+                    robot.value = left_turn
+                case "left":
+                    robot.value = right_turn
+                case "stop":
+                    robot.stop()
+
+
     except websockets.exceptions.ConnectionClosed:
         print("Connection closed")
+
+
+
 
 if __name__ == "__main__":
     # Start the WebSocket server
